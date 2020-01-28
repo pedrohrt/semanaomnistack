600261c01f25364c1d283b05e722cd59415ff3d2
pedrohrt <pedrohenrique.rodriguest@gmail.com>
2020-01-28 01:40:31 -0300

    Finalizado o mobile

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -5,6 +5,7 @@ import { MaterialIcons } from '@expo/vector-icons';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 
 import api from '../services/api'
+import {connect, disconnect, subscribeToNewDevs} from '../services/socket'
 
 function Main({ navigation }) {
 
@@ -36,6 +37,22 @@ function Main({ navigation }) {
     loadInitialPosition();
   }, []);
 
+  useEffect(() => {
+    subscribeToNewDevs(dev => setDevs([...devs, dev]))
+  }, [devs])
+
+  function setupWebSocket() {
+    disconnect();
+
+    const {latitude, longitude } = currentRegion;
+
+    connect(
+      latitude, 
+      longitude,
+      techs
+    )
+  }
+
   async function loadDevs() {
     const { latitude, longitude } = currentRegion;
 
@@ -47,6 +64,7 @@ function Main({ navigation }) {
       }
     });
     setDevs(response.data.devs);
+    setupWebSocket();
   }
 
   function handleRegionChanged(region) {
