6ed0a5915d4ffe91e83fee0a333f86eec53786b2
pedrohrt <pedrohenrique.rodriguest@gmail.com>
2020-01-28 00:31:54 -0300

    Correcao de erros

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,17 +1,95 @@
-import React from 'react';
-import { StyleSheet,  View, TextInput, TouchableOpacity } from 'react-native';
-import MapView from 'react-native-maps';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet,  Image, View, Text,TextInput, TouchableOpacity } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
 import { MaterialIcons } from '@expo/vector-icons';
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 
-function Main() {
+import api from '../services/api'
+
+function Main({ navigation }) {
+
+  const [devs, setDevs] = useState([]);
+  const [currentRegion, setCurrentRegion] = useState(null)
+  const [techs, setTechs] = useState('')
+
+  useEffect(() => {
+
+    async function loadInitialPosition() {
+      const { granted } = await requestPermissionsAsync()
+
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: true
+        })
+
+        const { latitude, longitude } = coords;
+
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04,
+        })
+      }
+    }
+
+    loadInitialPosition();
+  }, []);
+
+  async function loadDevs() {
+    const { latitude, longitude } = currentRegion;
+
+    const response = await api.get('/search', {
+      params: {
+        latitude,
+        longitude,
+        techs,
+      }
+    });
+    setDevs(response.data.devs);
+  }
+
+  function handleRegionChanged(region) {
+    setCurrentRegion(region);
+  }
+
+  if (!currentRegion) {
+    return null
+  }
 
 
   return (
     <>
       <MapView 
-        onRegionChangeComplete={() => {}} 
+        onRegionChangeComplete={handleRegionChanged}
+        initialRegion={currentRegion} 
         style={styles.map}
-      />
+      >
+        {devs.map(dev => (
+          <Marker 
+            key={dev._id}
+            coordinate={{
+              latitude: dev.location.coordinates[1], 
+              longitude: dev.location.coordinates[0],
+            }}>
+          <Image 
+            style={styles.avatar} 
+            source={{ uri: dev.avatar_url}} 
+          />
+
+          <Callout onPress={() => {
+            navigation.navigate('Profile', { github_username })
+          }}>
+            <View style={styles.callout}>
+              <Text style={styles.devName}>{dev.name}</Text>
+              <Text style={styles.devBio}>{dev.bio}</Text>
+              <Text style={styles.devTeches}>{dev.techs.join(', ')}</Text>
+            </View>
+          </Callout>
+        </Marker>
+        ))}
+
+      </MapView>
       <View style={styles.searchForm}>
         <TextInput 
           style={styles.searchInput}
@@ -19,10 +97,11 @@ function Main() {
           placeholderTextColor="#999"
           autoCapitalize="words"
           autoCorrect={false}
-          
+          value={techs}
+          onChangeText={setTechs}
         />
 
-        <TouchableOpacity style={styles.loadButton}>
+        <TouchableOpacity onPress={loadDevs} style={styles.loadButton}>
           <MaterialIcons name="my-location" size={20} color="#FFF" />
         </TouchableOpacity>
       </View>
@@ -39,8 +118,8 @@ const styles = StyleSheet.create({
     width: 54,
     height: 54,
     borderRadius: 4,
-    borderWidth: 4,
-    borderColor: '#FFF'
+    borderWidth: 2,
+    borderColor: '#9159c1'
   },
 
   callout: {
--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.105:4444',
+  baseURL: 'http://192.168.1.103:4444',
 });
 
 export default api;
